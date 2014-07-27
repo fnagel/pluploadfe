0892c10a8c6f1db12401f059dc8b1305b1803380
Felix Nagel <info@felixnagel.com>
2014-07-28 00:43:08 +0200

    update copyright year

--- pi1/class.tx_pluploadfe_pi1_wizicon.php
+++ pi1/class.tx_pluploadfe_pi1_wizicon.php
@@ -2,7 +2,7 @@
 /***************************************************************
  *  Copyright notice
  *
- *  (c) 2011 Felix Nagel <info@felixnagel.com>
+ *  (c) 2011-2014 Felix Nagel <info@felixnagel.com>
  *  All rights reserved
  *
  *  This script is part of the TYPO3 project. The TYPO3 project is
