f73d69aa436d0659d9e031fadb9c23553438a142
Felix Nagel <info@felixnagel.com>
2015-08-24 14:08:28 +0200

    Maintenance release: Adjust dependencies for TYPO3 7.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,10 +21,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'uploadfolder' => '1',
 	'createDirs' => '',
 	'clearCacheOnLoad' => 1,
-	'version' => '1.2.1-dev',
+	'version' => '1.2.1',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.3.99',
+			'typo3' => '6.2.0-7.4.99',
 			'cms' => '',
 		),
 		'conflicts' => array(),
