2e1ded649378abe86cec15e1422bda19466ac9e8
Felix Nagel <info@felixnagel.com>
2016-03-29 18:25:12 +0200

    [TASK] Remove outdated dependency to cms

--- ext_emconf.php
+++ ext_emconf.php
@@ -26,7 +26,6 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'php' => '5.4.0-7.0.99',
 			'typo3' => '6.2.0-7.6.99',
-			'cms' => '',
 		),
 		'conflicts' => array(),
 		'suggests' => array(
