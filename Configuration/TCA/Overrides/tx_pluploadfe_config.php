e4046abb29cc4f46046e7e692d9aabfc0cf5c0eb
Felix Nagel <info@felixnagel.com>
2016-08-14 22:44:20 +0200

    [BUGFIX] Fix TCA tab configuration for TYPO3 6.2

--- Configuration/TCA/Overrides/tx_pluploadfe_config.php
+++ Configuration/TCA/Overrides/tx_pluploadfe_config.php
@@ -10,3 +10,15 @@ if (version_compare(TYPO3_version, '7.5.0', '>=')) {
 	$GLOBALS['TCA']['tx_pluploadfe_config']['columns']['upload_path']['config']['size'] = '30';
 	$GLOBALS['TCA']['tx_pluploadfe_config']['columns']['upload_path']['config']['max'] = '255';
 }
+
+// Compatibility for TYPO3 6.2
+if (version_compare(TYPO3_version, '7.0', '<')) {
+	$GLOBALS['TCA']['tx_pluploadfe_config']['ctrl']['dividers2tabs'] = TRUE;
+
+	// Use old localization path
+	$path = 'frontend/Resources/Private/Language';
+	$GLOBALS['TCA']['tx_pluploadfe_config']['types']['0']['showitem'] =
+		str_replace($path, 'cms', $GLOBALS['TCA']['tx_pluploadfe_config']['types']['0']['showitem']);
+	$GLOBALS['TCA']['tx_pluploadfe_config']['palettes']['access']['showitem'] =
+		str_replace($path, 'cms', $GLOBALS['TCA']['tx_pluploadfe_config']['palettes']['access']['showitem']);
+}
