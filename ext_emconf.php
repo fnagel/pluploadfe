64babb3430a9db16231f7c3d926aad1d09ff5249
Felix Nagel <info@felixnagel.com>
2017-08-27 16:21:52 +0200

    [TASK] Remove TYPO3 6.2 from list of supported versions, update version to 2.0.0-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,11 +21,11 @@ $EM_CONF[$_EXTKEY] = array(
     'uploadfolder' => '1',
     'createDirs' => '',
     'clearCacheOnLoad' => 1,
-    'version' => '1.6.2-dev',
+    'version' => '2.0.0-dev',
     'constraints' => array(
         'depends' => array(
-            'php' => '5.4.0-7.0.99',
-            'typo3' => '6.2.0-8.7.99',
+            'php' => '5.6.0-7.0.99',
+            'typo3' => '7.6.0-8.7.99',
         ),
         'conflicts' => array(),
         'suggests' => array(
