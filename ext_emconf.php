f4f73f2be877c52b31888ab5053c16199d92cf26
Felix Nagel <info@felixnagel.com>
2018-11-08 09:12:39 +0100

    [TASK] Consider extension PHP 7.2 ready

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '2.0.1-dev',
     'constraints' => array(
         'depends' => array(
-            'php' => '5.6.0-7.1.99',
+            'php' => '5.6.0-7.2.99',
             'typo3' => '7.6.11-8.7.99',
         ),
         'conflicts' => array(),
