775ec10f7066a9959df86282a5508552d35baf15
Felix Nagel <info@felixnagel.com>
2020-03-17 11:02:09 +0100

    Minor maintenance release. TYPO3 10.3 support.
    Changelog: https://github.com/fnagel/pluploadfe/compare/4.0.1...4.0.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,11 +19,11 @@ $EM_CONF[$_EXTKEY] = [
     'state' => 'stable',
     'uploadfolder' => '1',
     'clearCacheOnLoad' => 1,
-    'version' => '4.0.2-dev',
+    'version' => '4.0.2',
     'constraints' => [
         'depends' => [
             'php' => '7.0.0-7.2.99',
-            'typo3' => '9.4.0-10.2.99',
+            'typo3' => '9.4.0-10.3.99',
         ],
         'conflicts' => [],
         'suggests' => [
