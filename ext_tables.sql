ac81fdd50b203538ea6a9303e71c68b2934893d5
Felix Nagel <info@felixnagel.com>
2021-11-08 23:54:22 +0100

    [TASK] Remove auto generated core DB fields
    https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/9.3/Feature-85160-AutoCreateManagementDBFieldsFromTCACtrl.html

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,15 +2,6 @@
 # Table structure for table 'tx_pluploadfe_config'
 #
 CREATE TABLE tx_pluploadfe_config (
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-	tstamp int(11) DEFAULT '0' NOT NULL,
-	crdate int(11) DEFAULT '0' NOT NULL,
-	cruser_id int(11) DEFAULT '0' NOT NULL,
-	deleted tinyint(4) DEFAULT '0' NOT NULL,
-	hidden tinyint(4) DEFAULT '0' NOT NULL,
-	starttime int(11) DEFAULT '0' NOT NULL,
-	endtime int(11) DEFAULT '0' NOT NULL,
 	title tinytext NOT NULL,
 	upload_path text,
 	extensions text,
@@ -18,12 +9,8 @@ CREATE TABLE tx_pluploadfe_config (
 	feuser_field tinytext NOT NULL,
 	save_session tinyint(3) DEFAULT '0' NOT NULL,
 	obscure_dir tinyint(3) DEFAULT '1' NOT NULL,
-	check_mime tinyint(3) DEFAULT '1' NOT NULL,
-	
-	PRIMARY KEY (uid),
-	KEY parent (pid)
+	check_mime tinyint(3) DEFAULT '1' NOT NULL
 );
-
 
 
 #
