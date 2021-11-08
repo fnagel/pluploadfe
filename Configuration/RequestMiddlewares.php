221f2f254cc0c7666e6adff51d497c8c489f0ee5
Felix Nagel <info@felixnagel.com>
2021-11-08 23:07:02 +0100

    [CLEANUP] Remove workaround for registering eID middleware before TYPO3 10.4
    See bafb3d83ead805ec48436ed7f7b4ee684ade607d

--- Configuration/RequestMiddlewares.php
+++ Configuration/RequestMiddlewares.php
@@ -8,9 +8,7 @@ return [
                 'typo3/cms-frontend/authentication',
             ],
             'before' => [
-                // @todo Remove fallback when TYPO3 9.2-10.3 is no longer needed
-                version_compare(TYPO3_version, '10.3', '<=')
-                    ? 'typo3/cms-frontend/site' : 'typo3/cms-frontend/site-resolver',
+                'typo3/cms-frontend/site-resolver',
             ],
         ],
     ],
