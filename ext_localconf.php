801f9937d002ead57be9b6ebe48242ab3384c113
Felix Nagel <info@felixnagel.com>
2021-11-17 18:51:34 +0100

    [FIX] Remove use statements in extension localconf and tables file
    Using use statements is only functional in TYPO3 11.4 and above, see:
    https://docs.typo3.org/m/typo3/reference-coreapi/master/en-us/ExtensionArchitecture/ConfigurationFiles/Index.html

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,27 +1,25 @@
 <?php
 
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-
 defined('TYPO3') || die();
 
 call_user_func(function ($packageKey) {
-    ExtensionManagementUtility::addPageTSConfig(
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
         '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:'.$packageKey.'/Configuration/TSconfig/page.tsconfig">'
     );
 
-    ExtensionManagementUtility::addUserTSConfig('
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('
         options.saveDocNew.tx_pluploadtest_config=1
     ');
 
     // Add plugin
-    ExtensionManagementUtility::addPItoST43(
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPItoST43(
         $packageKey,
         'Classes/Controller/Pi1Controller.php',
         '_pi1',
         'list_type',
         1
     );
-    ExtensionManagementUtility::addTypoScript(
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript(
         'FelixNagel.Pluploadfe',
         'setup',
         'plugin.tx_pluploadfe_pi1.userFunc = FelixNagel\\Pluploadfe\\Controller\\Pi1Controller->main'
--- ext_tables.php
+++ ext_tables.php
@@ -1,25 +1,20 @@
 <?php
 
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Core\Imaging\IconRegistry;
-use TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider;
-
 defined('TYPO3') || die();
 
 // Add config record
-ExtensionManagementUtility::allowTableOnStandardPages('tx_pluploadfe_config');
+TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_pluploadfe_config');
 
 // Add icons
 /* @var $iconRegistry \TYPO3\CMS\Core\Imaging\IconRegistry */
-$iconRegistry = GeneralUtility::makeInstance(IconRegistry::class);
+$iconRegistry = TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(TYPO3\CMS\Core\Imaging\IconRegistry::class);
 $iconRegistry->registerIcon(
     'extensions-pluploadfe-config',
-    BitmapIconProvider::class,
+    TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
     ['source' => 'EXT:pluploadfe/Resources/Public/Icons/icon_tx_pluploadfe_config.gif']
 );
 $iconRegistry->registerIcon(
     'extensions-pluploadfe-wizard',
-    BitmapIconProvider::class,
+    TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
     ['source' => 'EXT:pluploadfe/Resources/Public/Icons/Extension.png']
 );
