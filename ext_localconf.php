034f7971f16c093ba692df9070d6d54498533275
Felix Nagel <info@felixnagel.com>
2025-11-27 11:11:39 +0100

    [TASK] Replace deprecated template plugin registration
    https://docs.typo3.org/c/typo3/cms-core/main/en-us/Changelog/13.3/Deprecation-102821-ExtensionManagementUtilityaddPItoST43.html

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,34 +1,32 @@
 <?php
 
+use FelixNagel\Pluploadfe\Controller\Pi1Controller;
 use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 defined('TYPO3') || die();
 
-call_user_func(static function ($packageKey) {
+call_user_func(static function () {
     $versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
+
+    // @todo Remove this when TYPO3 12 is no longer relevant!
     // Only include user.tsconfig if TYPO3 version is below 13 so that it is not imported twice.
     if ($versionInformation->getMajorVersion() < 13) {
+        // @extensionScannerIgnoreLine
         ExtensionManagementUtility::addUserTSConfig(
             '@import "EXT:pluploadfe/Configuration/user.tsconfig"',
         );
     }
 
     // Add plugin
-    ExtensionManagementUtility::addPItoST43(
-        $packageKey,
-        'Classes/Controller/Pi1Controller.php',
-        '_pi1',
-        'list_type',
-        1
-    );
     ExtensionManagementUtility::addTypoScript(
         'FelixNagel.Pluploadfe',
         'setup',
-        'plugin.tx_pluploadfe_pi1.userFunc = FelixNagel\\Pluploadfe\\Controller\\Pi1Controller->main'
+        'plugin.tx_pluploadfe_pi1 = USER
+        plugin.tx_pluploadfe_pi1.userFunc = '. Pi1Controller::class.'->main'
     );
 
     // Add records to the search
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['livesearch']['pluploadfe'] = 'tx_pluploadfe_config';
-}, 'pluploadfe');
+});
