abdcb10e68d4d2bdbbb3eea92874eb5509b26058
Felix Nagel <info@felixnagel.com>
2025-12-20 16:44:19 +0100

    [CLEANUP] Remove user TSconfig include for TYPO3 v12

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,24 +1,11 @@
 <?php
 
 use FelixNagel\Pluploadfe\Controller\Pi1Controller;
-use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 defined('TYPO3') || die();
 
 call_user_func(static function () {
-    $versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
-
-    // @todo Remove this when TYPO3 12 is no longer relevant!
-    // Only include user.tsconfig if TYPO3 version is below 13 so that it is not imported twice.
-    if ($versionInformation->getMajorVersion() < 13) {
-        // @extensionScannerIgnoreLine
-        ExtensionManagementUtility::addUserTSConfig(
-            '@import "EXT:pluploadfe/Configuration/user.tsconfig"',
-        );
-    }
-
     // Add plugin
     ExtensionManagementUtility::addTypoScript(
         'FelixNagel.Pluploadfe',
